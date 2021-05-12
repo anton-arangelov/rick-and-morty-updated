b2e02fdc73870478bdc45a75b999c43814aecef0
anton-arangelov <68649791+anton-arangelov@users.noreply.github.com>
2021-05-12 19:07:43 +0300

    Delete src directory

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import signedSlice from "./signed-slice.js";
-
-const store = configureStore({
-  reducer: { signed: signedSlice.reducer },
-});
-
-export default store;
